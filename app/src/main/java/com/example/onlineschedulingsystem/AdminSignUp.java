f5d8957a3f53e3d0f756f483d72134da28d626b6
Roger Arthur William A. De Guzman <65663786+galmaeegi@users.noreply.github.com>
2022-02-07 21:56:02 +0800

    Changed "Password" to "New Password"

--- app/src/main/java/com/example/onlineschedulingsystem/AdminSignUp.java
+++ app/src/main/java/com/example/onlineschedulingsystem/AdminSignUp.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class adminSignUp extends AppCompatActivity {
+public class AdminSignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
