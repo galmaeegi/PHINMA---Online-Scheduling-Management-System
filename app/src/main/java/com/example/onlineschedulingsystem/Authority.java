022ca973222a6beaf95eb02df24b08d3204be16f
Roger Arthur William A. De Guzman <65663786+galmaeegi@users.noreply.github.com>
2022-02-09 00:33:57 +0800

    Added many functions

---
+++ app/src/main/java/com/example/onlineschedulingsystem/Authority.java
@@ -0,0 +1,33 @@
+package com.example.onlineschedulingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Authority extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authority);
+
+        Button auth_btn_student = (Button) findViewById(R.id.auth_student);
+        auth_btn_student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Authority.this, Login.class));
+            }
+        });
+
+        Button auth_btn_admin = (Button) findViewById(R.id.auth_admin);
+        auth_btn_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Authority.this, AdminSignIn.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/onlineschedulingsystem/MainPage.java
@@ -0,0 +1,61 @@
+package com.example.onlineschedulingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        Button btn1 = (Button) findViewById(R.id.main_queue_btn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPage.this, Login.class));
+            }
+        });
+
+        Button btn2 = (Button) findViewById(R.id.main_schedule_btn);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPage.this, Transaction.class));
+            }
+        });
+
+        Button btn3 = (Button) findViewById(R.id.main_changepass_btn);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPage.this, ChangePassword.class));
+            }
+        });
+
+        Button btn4 = (Button) findViewById(R.id.main_logout_btn);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainPage.this, Login.class));
+            }
+        });
+
+        ImageButton smallSignOut = (ImageButton) findViewById(R.id.signout_button);
+
+        smallSignOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainPage.this,Login.class));
+
+            }
+        });
+
+
+    }
+}
