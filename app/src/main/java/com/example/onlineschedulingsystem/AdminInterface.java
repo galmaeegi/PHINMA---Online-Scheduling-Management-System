9ca6eb5b6233b705e2d63bdf4f8a7a925907f046
Daitooooo <ralphelasin912@gmail.com>
2022-03-03 11:25:11 +0800

    Commit

--- app/src/main/java/com/example/onlineschedulingsystem/AdminInterface.java
+++ app/src/main/java/com/example/onlineschedulingsystem/AdminInterface.java
@@ -13,8 +13,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -23,8 +26,8 @@ import java.util.Map;
 
 public class AdminInterface extends AppCompatActivity {
 
-    private Button Next;
-    private Button Rest;
+    private ImageButton Next;
+    private ImageButton Rest;
     private int counter;
     private TextView counterTxt;
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
@@ -34,9 +37,10 @@ public class AdminInterface extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
-            switch (view.getId()){
+            switch (view.getId()) {
                 case R.id.Load:
                     plusConter();
+
                     break;
                 case R.id.Rest:
                     initCounter();
@@ -44,11 +48,11 @@ public class AdminInterface extends AppCompatActivity {
             }
             String counter = counterTxt.getText().toString();
             root = FirebaseDatabase.getInstance().getReference().child("QUEUE");
-            root.child(String.valueOf(Integer.parseInt(counter+1)));
+            root.child(String.valueOf(Integer.parseInt(counter + 1)));
 
 
             HashMap<String, String> usermap = new HashMap<>();
-            usermap.put("Current Number",counter);
+            usermap.put("Current Number", counter);
             root.setValue(usermap);
 
         }
@@ -60,29 +64,49 @@ public class AdminInterface extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_interface);
 
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("QUEUE");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            ///Setting data in TextView///
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String data = dataSnapshot.child("Current Number").getValue().toString();
+                counterTxt.setText(data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         ImageButton smallSignOut = (ImageButton) findViewById(R.id.signout_button);
         smallSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(AdminInterface.this,Login.class));
+                startActivity(new Intent(AdminInterface.this, Login.class));
             }
         });
 
         counterTxt = (TextView) findViewById(R.id.countertxt);
-        Next = (Button) findViewById(R.id.Load);
+        Next = (ImageButton) findViewById(R.id.Load);
         Next.setOnClickListener(clickListener);
-        Rest = (Button) findViewById(R.id.Rest);
+        Rest = (ImageButton) findViewById(R.id.Rest);
         Rest.setOnClickListener(clickListener);
         initCounter();
 
     }
+
     private void initCounter() {
         counter = 0;
         counterTxt.setText(counter + "");
 
     }
-    private void plusConter(){
+
+    private void plusConter() {
         counter++;
         counterTxt.setText(counter + "");
+        onResume();
     }
 }
